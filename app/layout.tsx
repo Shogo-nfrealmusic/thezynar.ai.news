84ee8cdcf2f49b175b980d092c249bb8439b4651
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-03-08 19:21:57 +0900

    height

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <main className="min-h-screen pt-[var(--header-height)]">
+          {children}
+        </main>
       </body>
     </html>
   );
